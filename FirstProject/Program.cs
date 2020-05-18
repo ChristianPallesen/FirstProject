ce36304274a192162bf8e68c11bca4cf4fa95fe5
Christian Pallesen <chri31h3@edu.mercantec.dk>
2020-05-18 08:43:28 +0200

    Start of my first Project

---
+++ FirstProject/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Hej = "Hello World.";
+            Console.WriteLine(Hej);
+
+
+
+            Console.WriteLine("Skriv dit fornavn:");
+
+            string fornavn = Console.ReadLine();
+
+            Console.WriteLine("Skriv dit efternavn:");
+
+            string efternavn = Console.ReadLine();
+
+            Console.WriteLine("Dit navn er : " + fornavn + " " + efternavn);
+
+        }
+    }
+}
